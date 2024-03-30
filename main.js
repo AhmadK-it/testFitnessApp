4649fe8c98e5630e55f4bc3d1f2b5441574be9a1
AhmadK-it <76110939+AhmadK-it@users.noreply.github.com>
2024-03-30 18:32:58 +0200

    Update main.js
    removing hostname

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
 const router = require('./routers/router.js')
-const hostname = process.env.HOSTNAME || '127.0.0.1'
 const port = process.env.PORT || 3000
 app.use(router)
 
 
-app.listen(port, hostname,()=>{
-  console.log(`server is runing on : http://${hostname}:${port}/`)
+app.listen(port,()=>{
+  console.log(`server is runing on port : ${port} `)
 })
