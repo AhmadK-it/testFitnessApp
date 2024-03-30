a3673b312ffa717e9da9418b58c4bcff7d50ef16
AhmadK-it <76110939+AhmadK-it@users.noreply.github.com>
2024-03-30 18:23:03 +0200

    Update main.js
    fixing hostname and port for deployment

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const router = require('./routers/router.js')
-
+const hostname = process.env.HOSTNAME || '127.0.0.1'
+const port = process.env.PORT || 3000
 app.use(router)
 
 
-app.listen(3000, '127.0.0.1',()=>{
-  console.log('server is runing on : http://127.0.0.1:3000/')
+app.listen(port, hostname,()=>{
+  console.log(`server is runing on : http://${hostname}:${port}/`)
 })
